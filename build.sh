7682a44914d191271035d9ba09760563957be989
wddorch <92806422+wddorch@users.noreply.github.com>
2021-11-26 11:34:41 -0500

    master changes

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 tar cvzf bundle.tgz main.js bundle.json find-replace-stream.js
 /usr/local/bin/akamai ew upload 6689 --bundle bundle.tgz
-/usr/local/bin/akamai ew activate 6689 production 0.8
+/usr/local/bin/akamai ew activate 6689 production 0.9
