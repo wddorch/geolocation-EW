29dd06028d4b799b97b6c30fb3519718742cf0de
wddorch <92806422+wddorch@users.noreply.github.com>
2021-12-01 21:30:38 -0500

    added pwd & ls

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+pwd
+ls
 tar -cvzf "/Users/wdorch/Documents/SOASTA/Product/Edge Workers/duanedorch-com/bundle.tgz" "/Users/wdorch/Documents/SOASTA/Product/Edge Workers/duanedorch-com/main.js" "/Users/wdorch/Documents/SOASTA/Product/Edge Workers/duanedorch-com/bundle.json" "/Users/wdorch/Documents/SOASTA/Product/Edge Workers/duanedorch-com/find-replace-stream.js"
 /usr/local/bin/akamai ew upload 6689 --bundle "/Users/wdorch/Documents/SOASTA/Product/Edge Workers/duanedorch-com/bundle.tgz" --accountkey B-M-1YX7F48
 /usr/local/bin/akamai ew activate 6689 production 0.11 --accountkey B-M-1YX7F48
