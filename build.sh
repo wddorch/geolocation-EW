4f7dc052e2e24fbab38bd186520ed9078aaca87e
wddorch <92806422+wddorch@users.noreply.github.com>
2021-12-01 20:55:06 -0500

    added zsh path to jenkins

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 tar cvzf bundle.tgz main.js bundle.json find-replace-stream.js
 /usr/local/bin/akamai ew upload 6689 --bundle "/Users/wdorch/Documents/SOASTA/Product/Edge Workers/duanedorch-com/bundle.tgz" --accountkey B-M-1YX7F48
-/usr/local/bin/akamai ew activate 6689 production 0.10 --accountkey B-M-1YX7F48
+/usr/local/bin/akamai ew activate 6689 production 0.11 --accountkey B-M-1YX7F48
