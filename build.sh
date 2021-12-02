b920b16f924623b56fcfd740805c6ab46c66ab64
wddorch <92806422+wddorch@users.noreply.github.com>
2021-12-01 20:29:23 -0500

    added full pathname of bundle file

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 tar cvzf bundle.tgz main.js bundle.json find-replace-stream.js
-/usr/local/bin/akamai ew upload 6689 --bundle bundle.tgz --accountkey B-M-1YX7F48
+/usr/local/bin/akamai ew upload 6689 --bundle /Users/wdorch/Documents/SOASTA/Product/Edge Workers/duanedorch-com/bundle.tgz --accountkey B-M-1YX7F48
 /usr/local/bin/akamai ew activate 6689 production 0.9 --accountkey B-M-1YX7F48
