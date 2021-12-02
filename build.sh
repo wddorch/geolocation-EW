79b2f3d243d35f58cf1789db213f3f40ef075bc1
wddorch <92806422+wddorch@users.noreply.github.com>
2021-12-01 20:26:19 -0500

    added account key

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 tar cvzf bundle.tgz main.js bundle.json find-replace-stream.js
-/usr/local/bin/akamai ew upload 6689 --bundle bundle.tgz
-/usr/local/bin/akamai ew activate 6689 production 0.9
+/usr/local/bin/akamai ew upload 6689 --bundle bundle.tgz --accountkey B-M-1YX7F48
+/usr/local/bin/akamai ew activate 6689 production 0.9 --accountkey B-M-1YX7F48
